3bf695bb6c450cb81bed229494ad78acc11c0d3b
jesusE <jesemil0709@gmail.com>
2023-12-09 00:06:26 -0600

    java app files managment modified

--- InterfazMCNC/src/interfazmcnc/InterfazMCNC.java
+++ InterfazMCNC/src/interfazmcnc/InterfazMCNC.java
@@ -12,12 +12,16 @@ public class InterfazMCNC {
         //pantalla.setLocation(50, 100);
         //pantalla.setSize(102,600);
         
-        String dockerComposeFile = System.getProperty("user.dir").replace("/InterfazMCNC","");
-
+        String dockerComposeFile = System.getProperty("user.dir").replace("/InterfazMCNC","/python_scripts");
+        if (dockerComposeFile.contains("dist")){
+            dockerComposeFile = dockerComposeFile.replace("/dist","");
+        }
         // Construye el comando para ejecutar docker-compose
         ProcessBuilder upDocker = new ProcessBuilder(
                 "docker-compose", "up","-d");
         upDocker.directory(new java.io.File(dockerComposeFile));
+        upDocker.redirectErrorStream(true);
+        upDocker.inheritIO();
         
         try {
             Process upDockerProcess = upDocker.start();
