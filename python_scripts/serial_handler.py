081286c528dabc3d3c74ccbb6fa41006b62ec0a3
jesusE <jesemil0709@gmail.com>
2023-12-18 03:22:27 -0600

    script para mantener comunicación serial abierta

--- python_scripts/serial_handler.py
+++ python_scripts/serial_handler.py
@@ -2,26 +2,42 @@ import serial
 import time
 import argparse
 import sys
+from serial.serialutil import SerialException
 
 parser = argparse.ArgumentParser(description='Serial reading')
 parser.add_argument('serial_port',
         help='serial device path')
-parser.add_argument('-s','--stop',action='store_true', default=False, 
-        help='stop serial communication')
 
 args = parser.parse_args()
-
-puerto_serial = serial.Serial(args.serial_port, 115200)  # Ajusta el nombre del puerto según tu configuración
+puerto_serial = serial.Serial(args.serial_port, 115200, timeout=2)  # Ajusta el nombre del puerto según tu configuración
+time.sleep(2)   # Wait for grbl to initialize 
+puerto_serial.flushInput()  # Flush startup text in serial input
 
 try:
-    while True:
-        # Tu lógica para leer/escribir en el puerto serial
-        data = puerto_serial.readline()
-        print(data.decode('utf-8'), flush=True)
-        sys.stdout.flush()
-        time.sleep(1)
+    while puerto_serial.is_open:
+        # Your code here
+        pass
+except KeyboardInterrupt:
+    # Handle keyboard interrupt if needed
+    pass
+finally:
+    # Make sure to close the serial port when done
+    if puerto_serial.is_open:
+        puerto_serial.close()
+    print('Comunicación serial se cerro')
 
+'''
+try:
+    while True:
+        try :
+            data = puerto_serial.readline()
+            if data.decode('utf-8').strip() != '' :
+                print(data.decode('utf-8').strip())
+                sys.stdout.flush()
+        except SerialException as e :
+              print(e)
 except KeyboardInterrupt:
     print("Saliendo del bucle infinito...")
     sys.stdout.flush()
     puerto_serial.close()
+'''
